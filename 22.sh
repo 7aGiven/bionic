1ddeca5a9a8e39ba2c51c7c02f5dd3e06a49eaa2
7aGiven <fredrickareichert@gmail.com>
2025-02-05 08:49:38 +0800

    dash

--- 22.sh
+++ 22.sh
@@ -1,3 +1,4 @@
+#!/bin/dash
 branch=android-5.1.1_r38
 projects='build bionic external/compiler-rt external/jemalloc external/libcxx external/stlport prebuilts/clang/linux-x86/host/3.5 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8 prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.6 system/core'
 
