7df8c7fc70f5e190bd6338c439cb1566a8bdc52b
7aGiven <fredrickareichert@gmail.com>
2025-02-07 15:36:10 +0800

    gtest

--- 22.sh
+++ 22.sh
@@ -1,6 +1,6 @@
 #!/bin/dash
 branch=android-5.1.1_r38
-projects='build bionic external/compiler-rt external/jemalloc external/libcxx external/stlport prebuilts/clang/linux-x86/host/3.5 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8 prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.6 system/core'
+projects='build bionic external/compiler-rt external/gtest external/jemalloc external/libcxx external/stlport prebuilts/clang/linux-x86/host/3.5 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8 prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.6 prebuilts/ndk system/core'
 
 rm -rf $branch
 mkdir $branch
